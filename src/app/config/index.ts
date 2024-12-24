eaed1f7bda5362071885c13f67ab8e9984ebe890
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-12-24 13:03:37 +0600

    Project Init

---
+++ src/app/config/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  client_url: process.env.CLIENT_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  default_password: process.env.DEFAULT_PASSWORD,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  jwt_reset_secret: process.env.JWT_RESET_SECRET,
+  sendgrid_api_key: process.env.SENDGRID_API_KEY,
+  sendgrid_email: process.env.SENDGRID_EMAIL,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+};
